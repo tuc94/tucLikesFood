04102233afe85810aee300ed49657b4dbebf43c6
Tuc Baldecchi <tucbaldecchi@tucs-mbp.myfiosgateway.com>
2021-10-20 16:13:40 -0400

    stuck on OnChange for slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Slider from "@material-ui/core/Slider";
+import { useState } from 'react';
 
 function App() {
+  const [score, setScore] = useState("5");
+  const marks = [
+    {
+      value: 1,
+      label: '1',
+    },
+    {
+      value: 2,
+      label: '2',
+    },
+    {
+      value: 3,
+      label: '3',
+    },
+    {
+      value: 4,
+      label: '4',
+    },
+    {
+      value: 5,
+      label: '5',
+    },
+    {
+      value: 6,
+      label: '6',
+    },
+    {
+      value: 7,
+      label: '7',
+    },
+    {
+      value: 8,
+      label: '8',
+    },
+    {
+      value: 9,
+      label: '9',
+    },
+    {
+      value: 10,
+      label: '10',
+    }
+  ];
+  
+  function valuetext(value:any) {
+    return `${value}°C`;
+  }
+
+  function handleChange (event: React.FormEvent<HTMLInputElement>,newValue: number | number[]) {
+    setScore( event.currentTarget.value );
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"IMG-1107.jpg"} alt="" width="500" height="600"/>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Rating this meal from 0-10
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="slider">
+        <Slider
+        aria-label="Small steps"
+        defaultValue={5.0}
+        getAriaValueText={valuetext}
+        step={0.1}
+        marks
+        min={0}
+        max={10}
+        valueLabelDisplay="auto"
+        onChange={ (e: React.FormEvent<HTMLInputElement>, val) => setScore(e.currentTarget.value) } 
+      />
+      <p>{score}</p>
+      </div>
       </header>
     </div>
   );
